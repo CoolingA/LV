02f9bdd9a2753f4cebe099ecc09e34e0635446b0
Kakou_Kuraki <kakou.kuraki@gmail.com>
2022-06-29 20:44:16 +0900

    first timeline impl

--- draw/src/timeline.rs
+++ draw/src/timeline.rs
@@ -1,3 +1,8 @@
+use objects;
+
+type Vector3 = cgmath::Vector3<f32>;
+type Vector4 = cgmath::Vector4<f32>;
+
 pub enum ObjectKind {
     Sphere,
     Cube,
@@ -25,8 +30,25 @@ pub struct Object {
     ry_end: f32,
     rz_start: f32,
     rz_end: f32,
+    sx_start: f32,
+    sx_end: f32,
+    sy_start: f32,
+    sy_end: f32,
+    sz_start: f32,
+    sz_end: f32,
+    cx_start: f32,
+    cx_end: f32,
+    cy_start: f32,
+    cy_end: f32,
+    cz_start: f32,
+    cz_end: f32,
+    red: f32,
+    green: f32,
+    blue: f32,
+    alpha: f32,
 }
 
+// objects must be ordered by time!
 pub struct Layer {
     objects: Vec<Object>,
 }
@@ -36,5 +58,68 @@ pub struct TimeLine {
 }
 
 impl Object {
-    pub draw(self);
+    pub fn gen(self, frame_num: i32) -> impl objects::Object {
+        let move_ratio = (frame_num - self.start) as f32 / (self.end - self.start) as f32;
+        let x = (self.x_end - self.x_start) * move_ratio;
+        let y = (self.y_end - self.y_start) * move_ratio;
+        let z = (self.z_end - self.z_start) * move_ratio;
+        let rx = (self.rx_end - self.rx_start) * move_ratio;
+        let ry = (self.ry_end - self.ry_start) * move_ratio;
+        let rz = (self.rz_end - self.rz_start) * move_ratio;
+        let sx = (self.sx_end - self.sx_start) * move_ratio;
+        let sy = (self.sy_end - self.sy_start) * move_ratio;
+        let sz = (self.sz_end - self.sz_start) * move_ratio;
+        let cx = (self.cx_end - self.cx_start) * move_ratio;
+        let cy = (self.cy_end - self.cy_start) * move_ratio;
+        let cz = (self.cz_end - self.cz_start) * move_ratio;
+        match self.kind {
+            Sphere => {
+                let sphere = objects::Sphere::new();
+                sphere.rescale_x(sx);
+                sphere.rescale_x(sy);
+                sphere.rescale_x(sz);
+                sphere.recolor(Vector4::new(self.red, self.green, self.blue, self.alpha));
+                sphere.generate_nodes();
+                sphere.translocate(Vector3::new(x, y, z));
+                sphere.rotate_x(rx, cy, cz);
+                sphere.rotate_y(ry, cx, cz);
+                sphere.rotate_z(rz, cx, cy);
+                sphere
+            },
+        }
+    }
+}
+
+impl Layer {
+    pub fn gen(self, frame_num: i32) -> Option(impl objects::Object) {
+        let existance = self.is_exist(frame_num);
+        if existance.0 {
+            return Some(self.objects[existance.1].gen(frame_num));
+        }
+        None
+    }
+
+    fn is_exist(self, frame_num: i32) -> (bool, i32) {
+        let mut object_index: i32 = 0;
+        for object in self.objects {
+            if object.end > frame_num {
+                break;
+            }
+            object_index += 1;
+        }
+        (self.objects[object_index] < frame_num, object_index)
+    }
+}
+
+impl TimeLine {
+    pub fn gen(self, frame_num: i32) -> Vec<impl objects::Object> {
+        let mut objects = vec![];
+        for layer in self.layers {
+            let may_object = layer.gen(frame_num);
+            if let Some(object) = may_object {
+                objects.push(object.gen(frame_num));
+            }
+        }
+        objects
+    }
 }
